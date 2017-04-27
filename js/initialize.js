0e8d42c93cd0c4cd8254e65a10bc004648d58100
schall11 <schall1186@gmail.com>
2017-04-27 15:02:46 -0600

    Added modal dialog with cookie

---
+++ js/initialize.js
@@ -0,0 +1,33 @@
+ require([
+        "dijit/Dialog",
+        "dojo/domReady!"
+    ], function(
+        Dialog
+    ) {
+    dojo.require("dojox.storage");
+    dojo.addOnLoad(function(){
+        dojox.storage.manager.initialize();
+        storageProvider=dojox.storage.manager.getProvider();
+        storageProvider.initialize();
+        var myObject = storageProvider.get("visitedBool");
+        if (myObject == false){
+            myDialog.show();
+        }
+    });
+   storeClose = function(){
+    dojox.storage.manager.initialize();
+    storageProvider=dojox.storage.manager.getProvider();
+    storageProvider.initialize();
+    var myObject={key1:true};
+    storageProvider.put("visitedBool", myObject, function(status, keyName){
+            // console.log("value put in "+keyName);
+    });
+    myDialog.hide();
+    };
+        myDialog = new Dialog({
+        title: "",
+        content: '<p>Click anywhere on the map or enter your address to see the nearest agents.</p><p>Use the slider on the right to change your search radius.</p><div class="dijitDialogPaneActionBar"><button data-dojo-type="dijit/form/Button" type="button" data-dojo-props="onClick:storeClose" id="cancel">OK</button> </div>',
+        style: "width: 450px"
+    });
+
+    });
